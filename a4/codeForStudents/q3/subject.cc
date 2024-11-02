4325fbef469d6ea44ac4d26467ca2b379553a1a3
jschmitz <jschmitz@uwaterloo.ca>
2024-11-01 22:16:49 -0400

    Releasing A4 to students

---
+++ a4/codeForStudents/q3/subject.cc
@@ -0,0 +1,15 @@
+#include "subject.h"
+#include "observer.h"
+#include <vector>
+
+void Subject::attach( Observer* o ) {
+    /*** FILL IN ***/
+}
+
+void Subject::detach( Observer* o ) {
+    /*** FILL IN ***/
+}
+
+void Subject::notifyObservers() {
+    /*** FILL IN ***/
+}
