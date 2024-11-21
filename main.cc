9676c4705d8de30787c84ac36a254841f1b3d304
a37thaku <a37thaku@uwaterloo.ca>
2024-11-20 23:10:33 -0500

    to see if git push is working

--- main.cc
+++ main.cc
@@ -4,4 +4,5 @@ int main() {
     std::cout << "LETS GO KEATS WAY" << std::endl;
     std::cout << "KEATS WAY ON TOP ONG lol" << std::endl;
     std::cout << "KEATS WAY ON TOP TYPE SHI" << std::endl;
+    std::cout << "This is to see if git push is working" << std::endl;
 }
