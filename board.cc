ba0b585f9670f4c6608981b3d70ab033a7a9f05a
a9dahiya <a9dahiya@uwaterloo.ca>
2024-11-24 18:42:52 -0500

    men

--- board.cc
+++ board.cc
@@ -54,7 +54,7 @@ Board::Board(std::vector<std::shared_ptr<Player>> players) : board(height, std::
 
 bool Board::hasOppLink(Position pos, std::shared_ptr<Player> player) {
    
-    auto cell = board[pos.getY()][pos.getX()];
+    auto cell = board[pos.getRow()][pos.getCol()];
     
     return cell->getLink() && !player->isOwnLink(cell->getLink());
 }
@@ -93,5 +93,31 @@ void Board::tatake(std::shared_ptr<Link> attacker, std::shared_ptr<Link> defende
     winner->getOwner()->downloadLink(loser);
 
     Position loserPos = loser->getPos();
-    removeLink(loser, loserPos);
+    removeLink(loserPos);
+}
+
+
+void Board::moveLink(std::shared_ptr<Link> link, Position from, Position to) {
+    if (!link) return;
+
+    auto cellFrom = getCell(from);
+    auto cellTo = getCell(to);
+
+    cellFrom->removeLink();
+    cellTo->setLink(link);
+    link->setPos(to);
+}
+
+std::shared_ptr<Cell> Board::getCell(Position pos) {
+    return board[pos.getRow()][pos.getCol()];
+}
+
+
+void Board::removeLink(Position pos) {
+    auto cell = getCell(pos);
+    if (cell->getLink()) {
+        cell->removeLink();
+    } else {
+        // shoot the error out daddy
+    }
 }
--- board.h
+++ board.h
@@ -14,17 +14,15 @@ public:
     Board(std::vector<std::shared_ptr<Player>> players);
     std::shared_ptr<Cell> getCell(Position pos);
     void moveLink(Position from, Position to);
-    void addLink(std::shared_ptr<Link> link, Position pos);
-    void removeLink(std::shared_ptr<Link> link, Position pos);
+    void removeLink(Position pos);
     bool ValidMove(Position from, Position to, std::shared_ptr<Player> player);
     bool hasOppLink(Position pos, std::shared_ptr<Player> player);
     bool isOppServer(Position pos, std::shared_ptr<Player> player);
     bool offEdge(Position pos, std::shared_ptr<Player> player);
     int getHeight() const;
     int getWidth() const;
-    void tatake(std::shared_ptr<Link> attacker, std::shared_ptr<Link> defender)
-
-    friend std::ostream& operator<<(std::ostream& out, const Board& board);
+    void tatake(std::shared_ptr<Link> attacker, std::shared_ptr<Link> defender);
+    void moveLink(std::shared_ptr<Link> link, Position from, Position to);
 
 private:
     int width = 8;
