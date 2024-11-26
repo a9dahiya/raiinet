6c9c21b45af21ca9d1445c0ca2bccfcfac4ee744
a37thaku <a37thaku@uwaterloo.ca>
2024-11-26 01:10:39 -0500

    textobserver type shit

--- TextObserver.cc
+++ TextObserver.cc
@@ -3,6 +3,6 @@ using namespace std;
 
 TextObserver::TextObserver(shared_ptr<GameState> subject, shared_ptr<Player> Viewer): subject{subject}, Viewer{Viewer}{}
 
-void TextObserver::notify() override {
+void TextObserver::notify() {
     cout << subject->GetBoard();
 }
--- main.d
+++ main.d
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 main.o: main.cc Player.h Link.h Position.h Ability.h subject.h Observer.h \
  GameState.h board.h Cell.h Firewall.h Polarize.h Scan.h Download.h \
+=======
+main.o: main.cc Player.h Link.h Position.h Ability.h GameState.h board.h \
+ Cell.h Firewall.h subject.h Observer.h Polarize.h Scan.h Download.h \
+>>>>>>> Stashed changes
  LinkBoost.h BattleGod.h UnlimitedVoid.h RussianRoulette.h \
  GraphicalObserver.h XWindow.h TextObserver.h
