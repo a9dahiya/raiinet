2b65a018edb9e98cefe454b21063cef10984eff6
hs2nair <hs2nair@uwaterloo.ca>
2024-11-25 19:35:43 -0500

    ability

--- Ability.h
+++ Ability.h
@@ -11,19 +11,18 @@ using namespace std;
 
 
 class Ability {
+    string name;
+    int id;
+    shared_ptr<Player> Owner;
+    bool used = false;
 public:
     Ability(string name, int id, shared_ptr<Player> Owner);
     virtual void execute(shared_ptr<GameState> game) = 0;
     bool isUsed();
     void setUsed();
     std::string getName();
+    shared_ptr<Player> getOwner();
     int getId();
-
-private:
-    string name;
-    int id;
-    shared_ptr<Player> Owner;
-    bool used = false;
 };
 
 #endif // ABILITY_H
