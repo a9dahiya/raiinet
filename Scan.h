8bf3e57cd9a5a2fcd97e676b4e0ab123c20ae641
Hrishi <hrishisn@gmail.com>
2024-11-25 23:58:01 -0500

    Jayz other abiltiies

---
+++ Scan.h
@@ -0,0 +1,17 @@
+#ifndef SCAN_H
+#define SCAN_H
+
+#include <memory>
+#include "Ability.h"
+
+
+class Scan : public Ability {
+    std::shared_ptr<Link> targetLink;
+public:
+    void execute(shared_ptr<GameState> game) override;
+    Scan(shared_ptr<Ability> ability, shared_ptr<Link> link);
+    // YASHILA IS OVER HERE AND DISHAS BD IS HERE AND SHE DIDNT INVITE HER WHAT DO U WANNA 
+    // 2 options, we can invite her to the daru party honestly, or we just vibes
+};
+
+#endif // SCAN_H
