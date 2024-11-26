b55bec667135705b9c1984af926b630ca5d194be
a37thaku <a37thaku@uwaterloo.ca>
2024-11-26 01:06:13 -0500

    ability fucked up

--- Ability.cc
+++ Ability.cc
@@ -1,7 +1,5 @@
 #include "Ability.h"
-#include <memory>
-#include <string>
-#include "Player.h"
+
 using namespace std;
 
 Ability::Ability(string name, int id, shared_ptr<Player> Owner): name{name}, id{id}, Owner{Owner} {}
--- Ability.h
+++ Ability.h
@@ -11,20 +11,11 @@ using namespace std;
 
 
 class Ability {
-public:
-    Ability(string name, int id, shared_ptr<Player> Owner);
-    Ability(std::shared_ptr<Ability> baseAbility);
-    virtual void execute(shared_ptr<GameState> game) = 0;
-    bool isUsed();
-    void setUsed();
-    std::string getName();
-    int getId();
-
-private:
     string name;
     int id;
     shared_ptr<Player> Owner;
     bool used = false;
+
 public:
     Ability(string name, int id, shared_ptr<Player> Owner);
     Ability(const Ability& baseAbility);
