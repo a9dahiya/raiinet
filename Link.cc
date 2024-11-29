19d30eacb7adca9710f8e4cb28651f9336351090
a9dahiya <a9dahiya@uwaterloo.ca>
2024-11-29 15:08:01 -0500

    added comments

--- Link.cc
+++ Link.cc
@@ -1,89 +1,114 @@
 #include "Link.h"
 
+// Constructor: Initializes a Link object.
+// Inputs: shared_ptr<Player> Owner - Owner of the Link,
+//         char letter - Link's unique letter identifier,
+//         string name - Link's name, 
+//         bool Data - True if Link is a Data Link, 
+//         int strength - Link's strength value.
+Link::Link(shared_ptr<Player> Owner, char letter, string name, bool Data, int strength)
+    : Owner{Owner}, letter{letter}, name{name}, Data{Data}, strength{strength} {}
 
-Link::Link(std::shared_ptr<Player> Owner, char letter, string name, bool Data, int strength)
-: Owner{Owner}, letter{letter}, name{name}, Data{Data}, strength{strength} {}
-
-std::shared_ptr<Player> Link::getOwner(){
+// Returns the owner of the Link.
+shared_ptr<Player> Link::getOwner() {
     return Owner;
 }
 
-int Link::getStrength(){
+// Returns the strength of the Link.
+int Link::getStrength() {
     return strength;
 }
 
-bool Link::isData(){
+// Returns true if the Link is a Data Link.
+bool Link::isData() {
     return Data;
 }
 
- Position Link::getPos(){
+// Returns the position of the Link on the board.
+Position Link::getPos() {
     return position;
- }
+}
 
- void Link::switchType(){
+// Switches the type of the Link (Data or Virus).
+void Link::switchType() {
     Data = !Data;
- }
+}
 
- bool Link::getLinkBoost(){
+// Returns true if the Link has a Link Boost.
+bool Link::getLinkBoost() {
     return hasLinkBoost;
- }
+}
 
- void Link::setLinkBoost(){
+// Sets the Link Boost status to true.
+void Link::setLinkBoost() {
     hasLinkBoost = true;
- }
+}
 
- bool Link::isBattleGod(){
+// Returns true if the Link is a Battle God.
+bool Link::isBattleGod() {
     return BattleGod;
- }
+}
 
- void Link::setBattleGod(){
+// Sets the Link as a Battle God.
+void Link::setBattleGod() {
     BattleGod = true;
- }
+}
 
- string Link::getName(){
+// Returns the name of the Link.
+string Link::getName() {
     return name;
- }
+}
 
-char Link::getLetter(){
+// Returns the unique letter identifier of the Link.
+char Link::getLetter() {
     return letter;
 }
 
- bool Link::isRevealed(){
+// Returns true if the Link is revealed to the opponent.
+bool Link::isRevealed() {
     return revealed;
- }
+}
 
-void Link::setRevealed(){
+// Sets the Link as revealed.
+void Link::setRevealed() {
     revealed = true;
 }
 
-bool Link::isActive(){
+// Returns true if the Link is active on the board.
+bool Link::isActive() {
     return onBoard;
 }
-void Link::setInactive(){
+
+// Sets the Link as inactive (removed from the board).
+void Link::setInactive() {
     onBoard = false;
 }
 
-void Link::setPos(Position pos){
+// Updates the Link's position on the board.
+// Inputs: Position pos - New position for the Link.
+void Link::setPos(Position pos) {
     position = pos;
 }
 
-bool Link::operator==(shared_ptr<Link> other){
-    if (getName() != other->getName() || getOwner() != other->getOwner()){
-        return false;
-    }
-    else{
-        return true;
-    }
+// Equality operator: Compares two Links based on their name and owner.
+// Inputs: shared_ptr<Link> other - The Link to compare against.
+// Outputs: bool - True if the Links are equivalent, false otherwise.
+bool Link::operator==(shared_ptr<Link> other) {
+    return (getName() == other->getName() && getOwner() == other->getOwner());
 }
 
-Position Link::getNewPos(string dir, int dist){
-    if (dir == "up"){
+// Calculates a new position based on direction and distance.
+// Inputs: string dir - Direction ("up", "down", "left", "right"),
+//         int dist - Distance to move.
+// Outputs: Position - The calculated new position.
+Position Link::getNewPos(string dir, int dist) {
+    if (dir == "up") {
         return Position{position.getRow() - dist, position.getCol()};
-    }else if (dir == "down"){
+    } else if (dir == "down") {
         return Position{position.getRow() + dist, position.getCol()};
-    }else if (dir == "left"){
+    } else if (dir == "left") {
         return Position{position.getRow(), position.getCol() - dist};
-    }else{
+    } else { // Assume "right" by default.
         return Position{position.getRow(), position.getCol() + dist};
     }
 }
--- UnlimitedVoid.cc
+++ UnlimitedVoid.cc
@@ -4,26 +4,45 @@
 
 using namespace std;
 
+// Constructor: Initializes an UnlimitedVoid ability.
+// Inputs: string name - The name of the ability,
+//         int id - Unique ID of the ability,
+//         shared_ptr<Player> owner - The owner of this ability.
 UnlimitedVoid::UnlimitedVoid(string name, int id, shared_ptr<Player> owner)
     : Ability{name, id, owner}, playerLink{nullptr}, opponentLink{nullptr} {}
 
+// Executes the UnlimitedVoid ability.
+// Inputs: shared_ptr<GameState> game - The current game state.
+// Outputs: bool - True if executed successfully, false otherwise.
 bool UnlimitedVoid::execute(shared_ptr<GameState> game) {
-    if(!playerLink || !opponentLink){
+    // Ensure both player's and opponent's links are set.
+    if (!playerLink || !opponentLink) {
         return false;
     }
+
+    // Access the game board and resolve the battle using the `tatakae` method.
     auto board = game->GetBoard();
     auto result = board->tatakae(playerLink, opponentLink);
-    setUsed();  
 
-    
-    cout << "Through Heaven and Earth, Link " << result->getLetter()  << " alone is the Honoured One" << std::endl;
+    // Mark the ability as used.
+    setUsed();
+
+    // Output the result of the battle.
+    cout << "Through Heaven and Earth, Link " 
+         << result->getLetter() 
+         << " alone is the Honoured One" << endl;
+
     return true;
 }
 
-void UnlimitedVoid::setMyLink(shared_ptr<Link> link){
+// Sets the player's link (attacking link).
+// Inputs: shared_ptr<Link> link - The link controlled by the player.
+void UnlimitedVoid::setMyLink(shared_ptr<Link> link) {
     playerLink = link;
 }
 
-void UnlimitedVoid::setOppLink(shared_ptr<Link> link){
+// Sets the opponent's link (defending link).
+// Inputs: shared_ptr<Link> link - The link controlled by the opponent.
+void UnlimitedVoid::setOppLink(shared_ptr<Link> link) {
     opponentLink = link;
 }
